34a53c4b6b3ed0e3d242684c383111c8bea6b976
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-04-30 12:16:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 function volume_sphere() {
-    //Write your code here
-	 const radiusInput = document.getElementById('radius').value;
-    
+    const radiusInput = document.getElementById('radius').value;
     const radius = parseFloat(radiusInput);
     
     if (isNaN(radius) || radius < 0) {
-
-		document.getElementById('volume').value = 'NaN';
+        document.getElementById('volume').value = 'NaN';
     } else {
-
-		const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-        
+        const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
         const roundedVolume = volume.toFixed(4);
-        
         document.getElementById('volume').value = roundedVolume;
     }
-  
-} 
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+    document.getElementById('MyForm').addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        volume_sphere(); 
+    });
+};
